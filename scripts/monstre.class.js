6932eaa515f456a97d157db036e8d08de9287f37
MelvilPRO <melvil.bouron@stagiairesmns.fr>
2023-03-27 13:13:45 +0200

    Transformer la casse creature en monstre(global)

--- scripts/monstre.class.js
+++ scripts/monstre.class.js
@@ -1,14 +1,114 @@
-import { Creature } from './creature.class.js';
+export class Monstre {
+    // Attributs des Créatures communes
+    #nom;       // STR
+    #degats;    // INT
+    #defense;   // INT
+    #health;    // INT
+    #images;    // [STR_PATH]
+    #target;    // CLASS INSTANCE MONSTRE/HEROS
 
-export class Monstre extends Creature {
-    #className;
+    // Variables statiques utiles
+    static maxHealth  = 100;
 
+    // Constructeur
     constructor(nom, degats, defense, health, images, target){
-        super(nom, degats, defense, health, images, target);
-        this.#className = "Monstre";
+        this.Init(nom, degats, defense, health, images, target);
     };
 
-    get GetClassName(){
-        return this.#className;
+    // Méthode Init appelée par le constructeur
+    Init(nom, degats, defense, health, images, target){
+        this.SetNom = nom;
+        this.SetDegats = degats;
+        this.SetDefense = defense;
+        this.SetHealth = health;
+        this.SetImages = images;
+        this.SetTarget = target;
+    }
+
+    // Méthode qui peut être appelée pour attaquer une cible
+    // Baisse les HPs de la cible en faisant : HPS + DEF - DEGATS;
+    // Renvoie true ou false si sa cible est morte
+    Attaque(cible){
+        if (cible.GetHealth > 0){
+            let HPS = cible.GetHealth;
+            let DEF = cible.GetDefense;
+            HPS = HPS + DEF - this.#degats;
+            cible.SetHealth = HPS;
+            console.log("La cible: " + cible.GetNom + " s'est faite attaquée, elle a maintenant " + HPS + " points de vie.");
+        }
+        if (cible.GetHealth <= 0){
+            console.log("La cible attaquée est morte");
+            return true;
+        }
+
+        return false;
+    }
+
+    AjouterDom(){
+
+    }
+
+    RetirerDom(){
+
+    }
+
+    LacherEquipement(nomEquipement){
+
+    }
+
+    get GetNom(){
+        return this.#nom;
+    }
+
+    set SetNom(nom){
+        this.#nom = nom;
+    }
+
+    get GetDegats(){
+        return this.#degats;
+    }
+
+    set SetDegats(degats){
+        this.#degats = degats;
+    }
+
+    get GetDefense(){
+        return this.#defense;
+    }
+
+    set SetDefense(defense){
+        this.#defense = defense;
+    }
+
+    get GetHealth(){
+        return this.#health;
+    }
+
+    set SetHealth(tmp){
+        if (tmp <= 0){
+            this.#health = 0;
+            console.log("Le monstre: " + this.#nom + " est décédée!");
+        } else if (tmp > Monstre.maxHealth){
+            this.#health = Monstre.maxHealth;
+            console.log("Les points de vie ne peuvent exceder: " + Monstre.maxHealth + " le monstre a maintenant: " + Monstre.maxHealth + " points de vie");
+        } else {
+            this.#health = tmp;
+        }
+    }
+
+    get GetImages(){
+        return this.#images;
+    }
+
+    set SetImages(images){
+        this.#images = images;
+    }
+
+    get GetTarget(){
+        return this.#target;
+    }
+
+    set SetTarget(target){
+        this.#target = target;
     }
 };
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 import { Heros } from './heros.class.js';
-import { Creature } from './creature.class.js';
 import { Monstre } from './monstre.class.js';
 
 let monHeros = new Heros("Jean", "Kevin", 11, 100, 8, 3);
@@ -9,8 +8,5 @@ monHeros.AjouterEquipement("arme");
 
 monHeros.UtiliserPotion();
 
-let randomCreature = new Creature("Zombie", 1, 4, 100, ["path1", "path2"], monHeros);
-console.log(randomCreature);
-
-let monster = new Monstre("MonsterEx", 2, 5, 100, ["path1"], monHeros);
-console.log(monster);
+let zombie = new Monstre("Zombie villageois", 2, 5, 100, ["pathImage1"], monHeros);
+console.log(zombie);
