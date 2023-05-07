d50e252fca4ee38b66d712bca19d06c0197cf027
MelvilPRO <127743721+MelvilPRO@users.noreply.github.com>
2023-05-07 11:02:50 +0200

    Transformation du script game.js en classe

--- assets/script/game.js
+++ assets/script/game.js
@@ -5,36 +5,68 @@ import { Boss } from './entities/boss.class.js';
 
 import { GameInterface } from './game.interface.js';
 
-let backgroundDom = document.querySelector("#background");
+export class Game {
+    #backgroundDom;
+    #alita;
 
-let alita = new Hero("Alita", "Zelielh");
-console.log(alita);
+    #gameInterface;
 
-function getRandomMonsters () {
-    let gameMonsters = [];
-
-    for (let i = 0; i < GameInterface.MaxStepNum; i++) {
-        let monsterType = Math.floor(Math.random() * Boss.MonsterType);
-        let monsterCurr;
-        switch (monsterType){
-            case Spider.MonsterType:
-                monsterCurr = new Spider(2, 3, 100, backgroundDom);
-                break;
-            case Skeleton.MonsterType:
-                monsterCurr = new Skeleton(3, 4, 100, backgroundDom);
-                break;
-            case Boss.MonsterType:
-                console.log("Le boss ne peut être placé qu'à la fin du tableau!");
-                break;
-            default:
-                console.log("Le type de monstre est inconnu!");
-        }
-
-        gameMonsters.push(monsterCurr);
+    constructor(){
+        this.backgroundDom = document.querySelector("#background");
+        this.alita = new Hero("Alita", "Zelielh");
+        this.gameInterface = new GameInterface(0, this.getRandomMonsters());
     }
 
-    gameMonsters.push(new Boss(4, 5, 100, backgroundDom));
-    return gameMonsters;
+    getRandomMonsters () {
+        let gameMonsters = [];
+    
+        for (let i = 0; i < GameInterface.MaxStepNum - 1; i++) {
+            let monsterType = Math.floor(Math.random() * Boss.MonsterType);
+            let monsterCurr;
+            switch (monsterType){
+                case Spider.MonsterType:
+                    monsterCurr = new Spider(2, 3, 100, this.backgroundDom);
+                    break;
+                case Skeleton.MonsterType:
+                    monsterCurr = new Skeleton(3, 4, 100, this.backgroundDom);
+                    break;
+                case Boss.MonsterType:
+                    console.log("Le boss ne peut être placé qu'à la fin du tableau!");
+                    break;
+                default:
+                    console.log("Le type de monstre est inconnu!");
+            }
+    
+            gameMonsters.push(monsterCurr);
+        }
+    
+        gameMonsters.push(new Boss(4, 5, 100, this.backgroundDom));
+        return gameMonsters;
+    }
+
+    get backgroundDom() {
+        return this.#backgroundDom;
+    }
+
+    set backgroundDom(tmp) {
+        this.#backgroundDom = tmp;
+    }
+
+    get alita() {
+        return this.#alita;
+    }
+
+    set alita(tmp) {
+        this.#alita = tmp;
+    }
+
+    get gameInterface() {
+        return this.#gameInterface;
+    }
+
+    set gameInterface(tmp) {
+        this.#gameInterface = tmp;
+    }
 }
 
-let gameInterface = new GameInterface(0, getRandomMonsters());
+let gameInstance = new Game();
