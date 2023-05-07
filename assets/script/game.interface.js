354c28ccdffb22ded5ba076e1df30ceeed4edef4
MelvilPRO <127743721+MelvilPRO@users.noreply.github.com>
2023-05-07 11:14:42 +0200

    Ajout de la méthode showYouLose

--- assets/script/game.interface.js
+++ assets/script/game.interface.js
@@ -3,13 +3,15 @@ export class GameInterface {
     #stepNum;
     static MaxStepNum = 10;
 
+    #loseDom;
+
     #gameMonsters;
 
     constructor(stepNum, gameMonsters){
         this.stepNum = stepNum;
         this.stepDom = document.querySelector("#step");
         this.#stepDom.innerHTML = "Etage: " + this.stepNum;
-
+        this.#loseDom = document.querySelector("#lose");
         this.gameMonsters = gameMonsters;
     }
 
@@ -21,6 +23,12 @@ export class GameInterface {
         } else {
             console.log("Vous avez atteint le niveau maximal, fin de la partie!");
         }
+    }
+
+    // Méthode utilisée lorsque vous avez perdu
+    showYouLose() {
+        this.loseDom.style.display = "block";
+        this.loseDom.style.textAlign = "center";
     }
 
     get stepDom() {
@@ -37,6 +45,14 @@ export class GameInterface {
 
     set stepNum(tmp) {
         this.#stepNum = tmp;
+    }
+
+    get loseDom() {
+        return this.#loseDom;
+    }
+
+    set loseDom(tmp) {
+        this.#loseDom = tmp;
     }
 
     get gameMonsters() {
